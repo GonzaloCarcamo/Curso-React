f6e7cecf9aca8c2772bc60139f3be616e831a126
GonzaloCarcamo <gon.cmo.dev@gmail.com>
2020-03-02 08:14:21 -0300

    repaso js intro

---
+++ 1. repaso-js/js/scope.js
@@ -0,0 +1,26 @@
+// scope
+
+/*
+var musica = 'rock';
+
+if (musica) {
+
+  var musica = 'grunge';
+  console.log('dentro del if:', musica);
+}
+
+  console.log ('fuera del if', musica)
+
+*/
+
+//...con let. También funciona con const
+
+let musica = 'rock';
+
+if (musica) {
+
+  let musica = 'grunge';
+  console.log('dentro del if:', musica);
+}
+
+  console.log ('fuera del if', musica);
---
+++ 1. repaso-js/js/var-const-let.js
@@ -0,0 +1,23 @@
+// variables con var
+
+var booleana = true;
+var number = 20;
+var string1 = 'Quiero aprender';
+var string2 = 'Javascript';
+
+
+console.log(string1 + ' ' + string2)
+console.log(booleana);
+
+// variables con const
+// las variables const no pueden crearse vacías, ni cambiar su valor
+
+const aprendiendo = true
+const curso = 'Javascript'
+
+// variables con let
+
+let prueba = false;
+string2 = 'React'
+
+console.log(string1 + ' ' + string2)
